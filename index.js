5c61b9c3b2ff0df37cdee9e92d9bfd9eb6e0fb51
Pini Clavero <maxi.a.clavero@gmail.com>
2022-11-28 21:25:52 -0300

    agregado e-commerce

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
+const app = require("./src/app")
 const express = require("express");
 const app = express();
 
 const port = process.env.PORT || 3000
 
 app.listen(port);
-app.get("/", (req,res)=>{
-    res.send("La Pagina de Inicio vercel")
-})
+
 console.log('listen on port ${port}')
